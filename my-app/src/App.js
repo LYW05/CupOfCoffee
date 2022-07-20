e4616812df86625dfb83a6e04342b135e8154d39
Lee <85977885+LYW05@users.noreply.github.com>
2022-07-19 21:53:15 -0700

    add routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(faCoffee);
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>My App</h1>
+      <h1>Cup of Coffee</h1>
+      <FontAwesomeIcon icon="coffee" />
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/home">Home</Link>
+        <Link to="/search">Search</Link>
+      </nav>
     </div>
   );
 }
-
-export default App;
---
+++ my-app/src/routes/home.jsx
@@ -0,0 +1,10 @@
+import '../App.css';
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
